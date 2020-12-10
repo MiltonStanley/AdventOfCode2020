5376313880d1abfd4e756ca9f42f4ae934f20302
Milton Stanley <milton.stanley@gmail.com>
2020-12-10 14:27:40 -0600

    Adding parsers for Day 2

--- Day2/day2.rb
+++ Day2/day2.rb
@@ -10,6 +10,24 @@ class DataLoader
 	end
 end
 
+class DataLineParser
+	def self.parse(line)
+		rule, password = line.split(': ')
+	end
+end
+
+class RuleParser
+	def self.parse(rule)
+		count, letter = rule.split(' ')
+	end
+end
+
+class PasswordTester
+	def self.test(rule, password)
+
+	end
+end
+
 class DataLoaderTest < Minitest::Test
 	def test_load_works
 		actual = DataLoader.load('day2_test_data.txt')
@@ -18,6 +36,22 @@ class DataLoaderTest < Minitest::Test
 			'1-3 b: cdefg',
 			'2-9 c: ccccccccc'
 		]
+		assert_equal(expected, actual)
+	end
+end
+
+class DataLineParserTest < Minitest::Test
+	def test_parse
+		expected = ['1-3 a', 'abcde']
+		actual = DataLineParser.parse('1-3 a: abcde')
+		assert_equal(expected, actual)
+	end
+end
+
+class RuleParserTest < Minitest::Test
+	def test_parse
+		expected = ['1-3','a']
+		actual = RuleParser.parse('1-3 a')
 		assert_equal(expected, actual)
 	end
 end
