f3d5de783097c410aecd83601599a479ac34160b
Milton Stanley <milton.stanley@gmail.com>
2020-12-11 10:30:13 -0600

    DataConverter#convert

--- Day3/day3.rb
+++ Day3/day3.rb
@@ -12,6 +12,22 @@ class DataLoader
   end
 end
 
+# Convert data from array of lines to 2d array of chars
+class DataConverter
+  def self.convert(data)
+    converted_data = []
+    data.each { |d| converted_data << convert_line(d) }
+    converted_data
+  end
+
+  private
+
+  # Returns array
+  def self.convert_line(line)
+    line.split('')
+  end
+end
+
 class DataLoaderTest < Minitest::Test
   def test_parse
     test_file = 'test_input.txt'
@@ -25,5 +41,29 @@ class DataLoaderTest < Minitest::Test
       expected = test_data[i]
       assert_equal(expected, actual[i])
     end
+  end
+end
+
+class DataConverterTest < Minitest::Test
+  def test_convert_line
+    line = '..##.......'
+    expected = %w[. . # # . . . . . . .]
+    converted_line = DataConverter.send(:convert_line,line)
+    assert_equal(expected, converted_line)
+  end
+
+  def test_convert
+    test_data = [
+      '..##.......',
+      '#...#...#..',
+      '.#....#..#.'
+    ]
+    expected = [
+      %w[. . # # . . . . . . .],
+      %w[# . . . # . . . # . .],
+      %w[. # . . . . # . . # .]
+    ]
+    actual = DataConverter.convert(test_data)
+    assert_equal(expected, actual)
   end
 end
