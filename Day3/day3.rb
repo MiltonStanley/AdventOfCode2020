e701d3d534432050cc0bf4760c2217a2808af465
Milton Stanley <milton.stanley@gmail.com>
2020-12-11 10:16:42 -0600

    Day3 initial commit & DataLoader

---
+++ Day3/day3.rb
@@ -0,0 +1,29 @@
+require 'minitest/autorun'
+
+# Loads data from file, returns array of lines
+class DataLoader
+  def self.load(file)
+    data = []
+    File.open(file).each do |line|
+      next if line.chomp.empty?
+      data << line.chomp
+    end
+    data
+  end
+end
+
+class DataLoaderTest < Minitest::Test
+  def test_parse
+    test_file = 'test_input.txt'
+    test_data = [
+      '..##.......',
+      '#...#...#..',
+      '.#....#..#.'
+    ]
+    test_data.each_index do |i|
+      actual = DataLoader.load(test_file)
+      expected = test_data[i]
+      assert_equal(expected, actual[i])
+    end
+  end
+end
