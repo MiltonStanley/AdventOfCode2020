92a82109a049db7402737a69142d10bc6d6db3b0
Milton Stanley <milton.stanley@gmail.com>
2020-12-12 10:52:47 -0600

    Removed old, bad code

--- Day3/day3.rb
+++ Day3/day3.rb
@@ -12,22 +12,6 @@ class DataLoader
   end
 end
 
-# Convert data from array of lines to 2d array of chars
-class DataConverter
-  def self.convert(data)
-    converted_data = []
-    data.each { |d| converted_data << convert_line(d) }
-    converted_data
-  end
-
-  private
-
-  # Returns array
-  def self.convert_line(line)
-    line.split('')
-  end
-end
-
 # Check if a given square has a tree; returns true (tree) or false (open)
 class TreeChecker
   def self.check(char)
@@ -35,32 +19,8 @@ class TreeChecker
   end
 end
 
-# Actual run the simulation, counting trees hit along the way
-class TobogganSimulator
-  def self.count(data)
-    location = {:row => -1, :col => -3}
-    trees_encountered = 0
-
-    data.each do |line|
-      max_row = data.length
-      max_col = line.length
-      
-      # Increment row, break if we're at the bottom
-      location[:row] += 1
-      break if location[:row] > max_row
-      # Increment col, loop it around if we go past the end
-      location[:col] += 3
-      if location[:col] > max_col
-        location[:col] -= max_col
-      end
-      trees_encountered += 1 if TreeChecker.check(line[location[:col]])
-    end
-    trees_encountered
-  end
-end
-
 # New, simple simulator
-class TobogganSimulatorNew
+class TobogganSimulator
   def self.count(data)
     increment = 3
     location = -3
@@ -87,30 +47,6 @@ class DataLoaderTest < Minitest::Test
       expected = test_data[i]
       assert_equal(expected, actual[i])
     end
-  end
-end
-
-class DataConverterTest < Minitest::Test
-  def test_convert_line
-    line = '..##.......'
-    expected = %w[. . # # . . . . . . .]
-    converted_line = DataConverter.send(:convert_line,line)
-    assert_equal(expected, converted_line)
-  end
-
-  def test_convert
-    test_data = [
-      '..##.......',
-      '#...#...#..',
-      '.#....#..#.'
-    ]
-    expected = [
-      %w[. . # # . . . . . . .],
-      %w[# . . . # . . . # . .],
-      %w[. # . . . . # . . # .]
-    ]
-    actual = DataConverter.convert(test_data)
-    assert_equal(expected, actual)
   end
 end
 
@@ -142,26 +78,3 @@ class TobogganSimulatorTest < Minitest::Test
     assert_equal(7, TobogganSimulator.count(test_data))
   end
 end
-
-class TobogganSimulatorNewTest < Minitest::Test
-  def test_count_trees
-    test_data = [
-      '..##.......',
-      '#...#...#..',
-      '.#....#..#.',
-      '..#.#...#.#',
-      '.#...##..#.',
-      '..#.##.....',
-      '.#.#.#....#',
-      '.#........#',
-      '#.##...#...',
-      '#...##....#',
-      '.#..#...#.#'
-    ]
-    assert_equal(7, TobogganSimulatorNew.count(test_data))
-  end
-end
-
-data = DataLoader.load('input.txt')
-
-puts TobogganSimulator.count(data)
