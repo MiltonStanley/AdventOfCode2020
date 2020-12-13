fb5ddb8ce10aa0ac76a6267b65c4ea6bb3160477
Milton Stanley <milton.stanley@gmail.com>
2020-12-13 15:04:07 -0600

    DataConverter#split_line_into_answers works

--- Day6/day6.rb
+++ Day6/day6.rb
@@ -23,7 +23,7 @@ class DataConverter
         group_answers[total_group_count] = []
         next
       end
-      
+      answers = split_line_into_answers(line)
     end
     group_answers
   end
@@ -31,15 +31,16 @@ class DataConverter
   private
 
   # First, split the line into individual answers into an array
-  def split_line_into_answers
+  def self.split_line_into_answers(line)
+    line.split('')
   end
 
   # Then, combine those with all the other answers
-  def combine_with_other_group_answers
+  def self.combine_with_other_group_answers
   end
 
   # Finally, clean up data by dropping duplicates
-  def clean_up_duplicates
+  def self.clean_up_duplicates
   end
 end
 ### TESTS ###
@@ -47,6 +48,17 @@ end
 class DataConverterTest < Minitest::Test
   def setup
     @data = DataLoader.load('test_input.txt')
+  end
+
+  def test_split_line_into_answers
+    {
+      'abc' => %w[a b c],
+      'ab' => %w[a b],
+      'a' => %w[a],
+    }.each do |line, expected|
+      actual = DataConverter.send(:split_line_into_answers, line)
+      assert_equal(expected, actual)
+    end
   end
 
   def test_has_correct_number_of_groups
