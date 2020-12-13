6a8543213b2b9e17bc31f5da67ebf369d4729bb0
Milton Stanley <milton.stanley@gmail.com>
2020-12-13 12:07:02 -0600

    More work converting a line to a hash

--- Day4/day4.rb
+++ Day4/day4.rb
@@ -22,14 +22,19 @@ class DataConverter
 
   # First, split the line into field pairs
   #   and return array of strings ['key:val', 'key:val'] etc
-  def self.split_into_key_val_pairs(line)
+  def self.split_into_fields(line)
     parts = line.split(' ')
   end
   
-  # Then, split the field pairs into key-val hash pairs
-  #   and return a hash.
+  # Then, split the array of fields into key-val hash pairs
+  #   and return a hash. 
   def self.compile_line(parts)
-
+    line_hash = {}
+    parts.each do |pair|
+      pair = convert_array_pair_to_key_val(pair)
+      line_hash.merge!(pair)
+    end
+    line_hash
   end
 
   # Convert 'key:val' to { 'key' => 'val' }
@@ -82,35 +87,43 @@ class DataConverterTest < Minitest::Test
   def test_split_into_pairs_1
     line = 'hcl:#ae17e1 iyr:2013'
     expected = %w[hcl:#ae17e1 iyr:2013]
-    actual = DataConverter.send(:split_into_key_val_pairs, line)
+    actual = DataConverter.send(:split_into_fields, line)
     assert_equal(expected, actual)
   end
 
   def test_split_into_pairs_2 
     line = 'iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884'
     expected = %w[iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884]
-    actual = DataConverter.send(:split_into_key_val_pairs, line)
+    actual = DataConverter.send(:split_into_fields, line)
     assert_equal(expected, actual)
   end
 
   def test_split_into_pairs_3
     line = 'eyr:2024'
     expected = %w[eyr:2024]
-    actual =DataConverter.send(:split_into_key_val_pairs, line)
+    actual = DataConverter.send(:split_into_fields, line)
     assert_equal(expected, actual)
   end
 
   def test_compile_line_1
-    line = 'hcl:#ae17e1 iyr:2013'
-    expected ={ 'hcl' => '#ae17e1', 'iyr' => '2013' }
+    line = %w[hcl:#ae17e1 iyr:2013]
+    expected = { 'hcl' => '#ae17e1', 'iyr' => '2013' }
     actual = DataConverter.send(:compile_line, line)
-    #assert_equal(expected, actual)
+    assert_equal(expected, actual)
   end
 
   def test_compile_line_2
+    line = %w[iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884]
+    expected = { 'iyr' => '2013', 'ecl' => 'amb', 'cid' => '350', 'eyr' => '2023', 'pid' => '028048884' }
+    actual = DataConverter.send(:compile_line, line)
+    assert_equal(expected, actual)
   end
 
   def test_compile_line_3
+    line = %w[eyr:2024]
+    expected = { 'eyr' => '2024' }
+    actual = DataConverter.send(:compile_line, line)
+    assert_equal(expected, actual)
   end
 
   def test_convert_array_pair_to_key_val
